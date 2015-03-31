767bb06af0c85eae87a0e214c9cba42693dc5985
Chaminda <9505636@student.swin.edu.au>
2015-03-31 20:04:23 +1100

    Initial commit on test

--- lib/robocop.rb
+++ lib/robocop.rb
@@ -1,5 +1,26 @@
 class Robocop
+	attr_accessor :x
+	attr_accessor :y
+	attr_accessor :direction
+
+	def initialize
+		@x = 0
+		@y = 0
+		@direction = :north
+	end
+
     def beep
         'beep boop'
+    end
+
+    def forward
+    if @direction = north 
+    	@y = @y + 1
+    	[@x,@y]
+
+    else @direction = east
+    	@x = @x + 1
+    	[@x,@y]
+    	
     end
 end
--- spec/robocop_spec.rb
+++ spec/robocop_spec.rb
@@ -1,8 +1,39 @@
 require 'robocop'
 
+
 RSpec.describe Robocop do
-  it "should beep" do
-    d = Robocop.new
-    expect(d.beep).to eq('beep boop')
-  end
+	describe '#forward' do
+		context 'starting at the default position' do
+			let(:robot) { Robocop.new }
+
+			it 'should move forward' do
+    			expect(robot.forward).to eq([0,1])
+			end
+		end
+
+		context 'starting at another position' do
+			let(:robot) {
+				robot = Robocop.new
+				robot.x = 100
+				robot.y = 120
+				return robot
+			}
+
+			it 'should move forward' do 
+				expect(robot.forward).to eq([100, 121])
+			end
+		end
+
+		context 'starting at another position and direction' do
+			let(:robot) { 
+				robot = Robocop.new
+				robot.direction = :east
+				return robot
+			}
+			
+			it 'should move forwards' do
+				expect(robot.forward).to eq([1,0])
+			end	
+		end
+	end
 end
