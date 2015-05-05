0897621b69d56bd10cfaeef1be8c12d6fd73cad3
Haze Soboh <hamzah@soboh.net>
2015-05-05 19:43:54 +1000

    Allowing user to select robot to move, also listing all robots

--- main.rb
+++ main.rb
@@ -4,26 +4,29 @@ require './lib/grid.rb'
 # This will clear the screen, so that it's easier to use
 system("clear")
 
-robo = Robocop.new(5, 0)
+robots = [Robocop.new(5, 0), Robocop.new(4, 2)]
 
 grid = Grid.new
 
 puts "\n\n\n"
-puts "Hi, I'm a moving robot, here is what we can do..."
+puts "Hi, I'm a robot moving system, here is what we can do..."
 puts "You can move me in 4 directions, they are as follows:"
 
 puts "\n\n"
 puts "\t\t******Navigation******"
-puts "\t\tf: will move me forwards*"
-puts "\t\tb: will move me backwards*"
-puts "\t\tr: will rotate me right*"
-puts "\t\tl: will rotate me left*"
-puts "\n\n"
-puts "\t\tgo to [x, y]: will move me from my current location to a new location*"
+puts "\t\t# f: will move me forwards*"
+puts "\t\t# b: will move me backwards*"
+puts "\t\t# r: will rotate me right*"
+puts "\t\t# l: will rotate me left*"
+puts "\n\t\twhere # is the robot id you would like to control\n"
+puts "\n"
+puts "\t\t******Admin******"
+puts "\t\tlist: will list all robot ids, locations, directions"
+# puts "\t\tgo to [x, y]: will move me from my current location to a new location*"
 puts "\n\n"
 puts "* 1 block within the CBD grid"
 puts "\n\n"
-puts "I am currently at #{grid.getStreet(robo.x, robo.y)} facing #{robo.direction}"
+# puts "I am currently at #{grid.getStreet(robo.x, robo.y)}, facing #{robo.direction}"
 puts "Please enter the command you would like me to carry out: "
 
 
@@ -33,24 +36,36 @@ while true
     userInput = gets.strip
 
     case userInput
-    when 'f'
-        robo.forward
-        puts "I am currently at #{grid.getStreet(robo.x, robo.y)}"
-        puts "I am currently facing #{robo.direction}"
+    when /[1-9]+\s*f/
+        selectedId = userInput.scan( /[1-9]+/).first.to_i - 1
+
+        if selectedId < robots.length
+            selectedRobot = robots[selectedId]
+            selectedRobot.forward
+            puts "I am currently at #{grid.getStreet(selectedRobot.x, selectedRobot.y)}, facing #{selectedRobot.direction}"
+        else
+            puts "Robot does not exist, please try again!"
+        end
+        
     when 'b'
         robo.backward
-        puts "I am currently at #{grid.getStreet(robo.x, robo.y)}"
-        puts "I am currently facing #{robo.direction}"
+        puts "I am currently at #{grid.getStreet(robo.x, robo.y)}, facing #{robo.direction}"
     when 'r'
         robo.rotateRight
         puts "I just rotated to my right"
-        puts "I am currently at #{grid.getStreet(robo.x, robo.y)}"
-        puts "I am currently facing #{robo.direction}"
+        puts "I am currently at #{grid.getStreet(robo.x, robo.y)}, facing #{robo.direction}"
     when 'l'
         robo.rotateLeft
         puts "I just rotated to my left"
-        puts "I am currently at #{grid.getStreet(robo.x, robo.y)}"
-        puts "I am currently facing #{robo.direction}"
+        puts "I am currently at #{grid.getStreet(robo.x, robo.y)}, facing #{robo.direction}"
+    when 'list'
+        puts "\n"
+        puts "id\t\tlocation\t\t\tdirection"
+        count = 0
+        robots.each { |r|
+            puts "#{count + 1}\t\t#{grid.getStreet(r.x, r.y)}\t#{r.direction}"
+            count += 1
+        }
 
     when /go to \[[1-9]+\s*,\s*[1-9]+\]/
         robo.rotateLeft
